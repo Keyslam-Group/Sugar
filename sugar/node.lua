ecee6b99d9721df2cf0dc532a911a6733bee64a6
Pablo Mayobre <pablo.mayobre@newcombin.com>
2025-04-02 22:25:45 -0300

    Update node.lua to match #1 API design

--- sugar/node.lua
+++ sugar/node.lua
@@ -4,22 +4,24 @@ function Node:new(props, buildFunction)
     self.props = props
     self.buildFunction = buildFunction
 
-    signal.effectWithId(self, self._build, self)
+    local this = self
+
+    self.effect = signal.effect(function ()
+        local buildResult = buildFunction(props)
+
+        -- If buildResult is a Node store the rootElement, if it's nil or Element, store the root as is
+        local newRootElement = buildResult and buildResult.rootElement or buildResult
+
+        this.rootElement.parentElement:replaceChild(this.rootElement, newRootElement)
+        this.rootElement:destroy()
+        this.rootElement = newRootElement
+    end)
 end
 
 function Node:destroy()
-    signal.destroyEffectWithId(self)
+    self.effect:destroy()
     self.rootElement.parentElement:removeChild(self.rootElement)
-end
-
-function Node:_build()
-    local buildResult = self.buildFunction(self.props)
-
-    -- If buildResult is a Node store the rootElement, if it's nil or Element, store the root as is
-    local newRootElement = buildResult and buildResult.rootElement or buildResult
-
-    self.rootElement.parentElement:replaceChild(self.rootElement, newRootElement)
-    self.rootElement = newRootElement
+    self.rootElement = nil
 end
 
 return Node
