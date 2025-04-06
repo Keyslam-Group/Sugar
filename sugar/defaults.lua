37483ed1fc1a13105e65794c744852afda1848c1
pablomayobre <pablo.a.mayobre@gmail.com>
2025-04-06 16:51:24 -0300

    Add a set of default values

---
+++ sugar/defaults.lua
@@ -0,0 +1,9 @@
+local defaults = {}
+
+defaults.defaultFont = love.graphics.getFont()
+defaults.defaultColor = {1, 1, 1, 1}
+defaults.defaultBackground = {0, 0, 0, 0}
+defaults.defaultBorderColor = {0, 0, 0, 0}
+defaults.defaultLineWidth = 1
+
+return defaults
